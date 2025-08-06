317f9754c9206864d2b29ef3c3a188f4e64b4f25
Ariel <112595798+Arielferm@users.noreply.github.com>
2025-08-06 19:59:12 -0300

    Update stock-price.py

--- stock-price-data-main/stock-price.py
+++ stock-price-data-main/stock-price.py
@@ -1,5 +1,4 @@
 
-
 import pandas as pd
 import yfinance as yf
 import datetime
@@ -17,28 +16,31 @@ start_date = d2
 # Collecting the stock price data of H&M using the yfinance API of Yahoo Finance.
 data = yf.download('HM-B.ST', start=start_date,
                               end=end_date,
-                              progress=False) 
+                              progress=False)
 
-print(data.head())  
+print(data.head())
+
+# Flatten the MultiIndex columns
+data.columns = ['_'.join(col).strip() for col in data.columns.values]
+print(data.columns)
 
 # "Date" is index in this dataset. Below is how to convert this index into column.
 data["Date"] = data.index
-data = data[["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]]
+data = data[["Date", "Open_HM-B.ST", "High_HM-B.ST", "Low_HM-B.ST", "Close_HM-B.ST", "Volume_HM-B.ST"]]
 data.reset_index(drop=True, inplace=True)
 print(data.head())
 
 # Candlestick shart of H&M's stock prices
-
-figure = go.Figure(data=[go.Candlestick(x=data["Date"], open=data["Open"], high=data["High"], low=data["Low"], close=data["Close"])])
+figure = go.Figure(data=[go.Candlestick(x=data["Date"], open=data["Open_HM-B.ST"], high=data["High_HM-B.ST"], low=data["Low_HM-B.ST"], close=data["Close_HM-B.ST"])])
 figure.update_layout(title = "H&M Stock Price Analysis", xaxis_rangeslider_visible=False)
 figure.show()
 
 # Bar plot which shows the close prices of H&M's stock
-figure = px.bar(data, x= "Date", y= "Close")
+figure = px.bar(data, x= "Date", y= "Close_HM-B.ST")
 figure.show()
 
 # Time period selectors are like buttons and show the graph of a specific time period
-figure = px.line(data, x='Date', y='Close', title='Stock Market Analysis with Time Period Selectors')
+figure = px.line(data, x='Date', y='Close_HM-B.ST', title='Stock Market Analysis with Time Period Selectors')
 
 figure.update_xaxes(
     rangeselector = dict(
